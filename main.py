588ad010669e6a7814eaa78b323313c0b2833c9f
ragq <ragq1991@gmail.com>
2021-05-27 14:03:35 +0600

    Done.

---
+++ main.py
@@ -0,0 +1,16 @@
+import hashlib
+
+def gen(path):
+    with open(path, "r") as data:
+        end = sum(1 for line in data)
+        data.seek(0)
+        for i in range(0, end):
+            if i > end:
+                raise StopIteration
+            hash = hashlib.md5(data.read(i).encode())
+            yield hash.hexdigest()
+
+
+if __name__ != 'main':
+    for i in gen('countries.txt'):
+        print(i)
